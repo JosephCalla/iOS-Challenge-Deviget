845d20f3efe79d8840f1135f1f88849b544e448a
Joseph Calla <jcallamoreyra@rccl.com>
2021-09-09 19:11:28 -0500

    iOS - Project created

---
+++ iOS-Challenge/AppDelegate.swift
@@ -0,0 +1,20 @@
+//
+//  AppDelegate.swift
+//  iOS-Challenge
+//
+//  Created by Joseph Calla Moreyra on 9/09/21.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+    
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+}
+
---
+++ iOS-Challenge/View/RedditPostViewController.swift
@@ -0,0 +1,29 @@
+//
+//  RedditPostViewController.swift
+//  iOS-Challenge
+//
+//  Created by Joseph Calla Moreyra on 9/09/21.
+//
+
+import UIKit
+
+class RedditPostViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
