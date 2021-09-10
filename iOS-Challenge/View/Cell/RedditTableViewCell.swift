6dc4c362e400d7992caeae1130c28ce36853f2bb
Joseph Calla <jcallamoreyra@rccl.com>
2021-09-10 17:46:36 -0500

    iOS - Show data

--- iOS-Challenge/View/Cell/RedditTableViewCell.swift
+++ iOS-Challenge/View/Cell/RedditTableViewCell.swift
@@ -16,8 +16,21 @@ class RedditTableViewCell: UITableViewCell {
     @IBOutlet weak var removeCellButton: UIButton!
     @IBOutlet weak var removeLabel: UILabel!
     
+    var post: RedditPostResponse? {
+        didSet {
+            setupCell(post: post)
+            }
+        }
+    
     override func awakeFromNib() {
         super.awakeFromNib()
         postImage.image = UIImage(named: "noImageFound")
+    }
+    
+    func setupCell(post: RedditPostResponse?) {
+        self.titleLabel.text = post?.title
+        self.authorLabel.text = post?.author
+        self.postImage.setImageFrom(link: post?.thumbnail)
+        self.commentsLabel.text = "\(post?.num_comments ?? 0) comments"
     }
 }
--- iOS-Challenge/View/RedditPostViewController.swift
+++ iOS-Challenge/View/RedditPostViewController.swift
@@ -9,7 +9,12 @@ import UIKit
 
 class RedditPostViewController: UIViewController {
     @IBOutlet weak var redditPostTableView: UITableView!
+    @IBOutlet weak var loadSpineer: UIActivityIndicatorView!
+    
+    private var refreshControl = UIRefreshControl()
+    private var selectedPostIndex = 0
     let viewModel = RedditViewModel()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         setupView()
@@ -35,14 +40,24 @@ class RedditPostViewController: UIViewController {
 
 extension RedditPostViewController: UITableViewDataSource {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 2
+        
+        return viewModel.posts?.count ?? 0
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = redditPostTableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath) as? RedditTableViewCell
+        let post = viewModel.posts?[indexPath.row]
+        cell?.post = post
         return cell!
     }
 }
 
 extension RedditPostViewController: UITableViewDelegate {
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 190
+    }
 }
